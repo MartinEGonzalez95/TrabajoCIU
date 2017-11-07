c5cb7f3f0ab01afd45c8fb4cf1d03175db7827ff
Gaston Testay <gaston.testay@gmail.com>
2017-11-06 23:52:27 -0300

    se agrego el metodo get pedidos anteriores de un usuario

--- WebRESTDominoPizza/src/main/webapp/js/services/PedidoService.js
+++ WebRESTDominoPizza/src/main/webapp/js/services/PedidoService.js
@@ -1,9 +1,26 @@
 angular.module('dominosApp').service('PedidoService', function($http){
 
+    let getData = function (response) {
+        return response.data
+    };
+
+
+    let parsearPedido = function (json) {
+        return new Pedido(json)
+    };
+
+    let parsearListaDePedidos = function (jsonDePedidos) {
+        return jsonDePedidos.map(parsearPedido)
+    };
+
+
    return{
        confirmarPedido: function (pedido){
 
-           return $http.post("pedidos",pedido)
+           return $http.post("pedidos",pedido);
+       },
+       obtenerPedidosAnterioresDe: function (pedido) {
+           return $http.get("pedidos?nick=" + pedido.cliente.nick).then(getData).then(parsearListaDePedidos);
        }
    }
 
