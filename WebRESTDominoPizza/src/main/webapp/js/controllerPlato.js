e16285dcb19cf6e5497b2397e0c3b1a9a8f21c24
Martin <martinegonzalez95@gmail.com>
2017-11-09 01:10:31 -0300

    Arregle varias cagadas que me habia mandado con Git

--- WebRESTDominoPizza/src/main/webapp/js/controllerPlato.js
+++ WebRESTDominoPizza/src/main/webapp/js/controllerPlato.js
@@ -1,24 +1,23 @@
-<<<<<<< HEAD
 angular.module('dominosApp').controller("controllerPlato", function ($state, MenuService, PedidoTemporalService, PlatoService) {
     
         const self = this;
 
         this.plato = PlatoService.plato;
 
-        this.ingredientes = PlatoService.plato.pizza.ingredientes;
+        this.ingredientes = PlatoService.plato.pizzaBase.ingredientes;
 
-        this.ingredientesExtras = []
+        this.ingredientesExtras = [];
 
-        this.obtenerIngredientes = function(){
-
-            MenuService.obtenerIngredientes()
-                .then(function(data){
-                    
-                    self.ingredientesExtras = data;
-    
-                })
-               // .catch(errorHandler);
-        }
+        this.obtenerIngredientes = function () {
+            
+                    MenuService.obtenerIngredientes()
+                        .then(function (data) {
+            
+                            self.ingredientesExtras = data;
+            
+                        })
+                    // .catch(errorHandler);
+                };
 
         this.obtenerIngredientes();
 
@@ -50,51 +49,4 @@ angular.module('dominosApp').controller("controllerPlato", function ($state, Men
         $state.go("confirmar")
     }
 
-=======
-angular.module('dominosApp').controller("controllerPlato", function ($state, MenuService, PedidoTemporalService, PlatoService) {
-
-    const self = this;
-
-    this.plato = PlatoService.plato;
-
-    this.ingredientes = PlatoService.plato.pizzaBase.ingredientes;
-
-    this.ingredientesExtras = [];
-
-    this.obtenerIngredientes = function () {
-
-        MenuService.obtenerIngredientes()
-            .then(function (data) {
-
-                self.ingredientesExtras = data;
-
-            })
-        // .catch(errorHandler);
-    };
-
-    this.obtenerIngredientes();
-
-    function errorHandler(error) {
-        console.log(error);
-        ExceptionService.capturarError(error);
-    }
-
-    this.agregar = function (unIngrediente) {
-        this.plato.ingredientesExtras.push(unIngrediente)
-    };
-
-    this.quitar = function (unIngrediente) {
-        var index = this.plato.ingredientesExtras.indexOf(unIngrediente);
-        if (index > -1) {
-            this.plato.ingredientesExtras.splice(index, 1);
-        }
-
-    };
-
-    this.confirmarme = function () {
-        PedidoTemporalService.agregarPlato(this.plato);
-        $state.go("confirmar")
-    }
-
->>>>>>> origin/dev_Web
 });
--- WebRESTDominoPizza/src/main/webapp/js/controllerTamanioPlato.js
+++ WebRESTDominoPizza/src/main/webapp/js/controllerTamanioPlato.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('dominosApp').controller('controllerTamanioPlato', function ($state,ExceptionService, TamanioService, PlatoService) {
 
 
@@ -18,55 +17,7 @@ angular.module('dominosApp').controller('controllerTamanioPlato', function ($sta
             })
             .catch(errorHandler);
     };
-
-    this.cargarTamanios();
-
-
-    this.agregarTamanioAlPlato = function (unTamanio) {
-
-        this.plato.tamanio = unTamanio;
-        platoService.agregarPlato(this.plato);
-
-
-        $state.go("ingredientes");
-
-    };
-
-
-    this.calcularTamanio = function (unTamanio) {
-        this.plato.tamanio = unTamanio;
-        return this.plato.precioPlato() || 0
-    };
-
-    function errorHandler(error) {
-        ExceptionService.capturarError(error);
-    }
-
-    this.excepciones = ExceptionService.errores;
-
-});
-
-
-=======
-angular.module('dominosApp').controller('controllerTamanioPlato', function ($state,ExceptionService, TamanioService, PlatoService) {
-
-
-    let platoService = PlatoService;
-
-    const self = this;
-
-    this.plato = platoService.plato;
-
-    this.tamanios = [];
-
-
-    this.cargarTamanios = function () {
-        TamanioService.obtenerTamanios()
-            .then(function (data) {
-                self.tamanios = data;
-            })
-            .catch(errorHandler);
-    };
+    
     this.cargarTamanios();
 
 
@@ -95,4 +46,3 @@ angular.module('dominosApp').controller('controllerTamanioPlato', function ($sta
 });
 
 
->>>>>>> origin/dev_Web
